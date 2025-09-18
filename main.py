d5954de1dcbea147322bab1f717228d03f5d57e8
pocafup <61262221+pocafup@users.noreply.github.com>
2025-09-18 15:18:50 -0700

    Adding initial file system structures
    * Initialize README
    
    * change format
    
    * change
    
    * setup file structures
    
    ---------
    
    Co-authored-by: pocafup <pocafup@pocafupdeMacBook-Air.local>

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import fastapi
+
+"""
+File Structures:
+root/
+    client/
+        Dockerfile 
+        src/
+            assets/
+            components/
+                navigation_bar.py
+            pages/
+                login.py
+                signup.py
+            utils/
+            app.py
+    server/
+        Dockerfile
+        src/
+            assets/
+            config/
+                db.py
+                redis.py
+            controllers/
+                 
+            middleware/
+                authMiddleware.py
+            model/
+                user.py
+            routes/
+            services/
+            tools/
+            utils/
+                authUtil.py
+                responseHandler.py
+        server.py
+        app.py
+    docker-compose.yml
+    main.py
+    poetry.lock
+    pyproject.toml
+
+"""
+
+
+if __name__ == "main":
+    return
